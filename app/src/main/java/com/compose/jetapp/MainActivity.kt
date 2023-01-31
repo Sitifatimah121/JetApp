b519589c0448f040c5721e3f6a9deb024943bf31
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-01-31 22:54:08 +0700

    [Banner] Show SearchBar

--- app/src/main/java/com/compose/jetapp/MainActivity.kt
+++ app/src/main/java/com/compose/jetapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.compose.jetapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.compose.components.SearchBar
 import com.compose.jetapp.ui.theme.JetAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +29,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
 
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier){
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true)
