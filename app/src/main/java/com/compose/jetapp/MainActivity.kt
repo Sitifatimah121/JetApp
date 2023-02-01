041110560ec226681ca02697286453af67c3afb6
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-02-01 22:50:25 +0700

    [Category Section] Update layout (Main Activity)

--- app/src/main/java/com/compose/jetapp/MainActivity.kt
+++ app/src/main/java/com/compose/jetapp/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.compose.components.CategoryItem
 import com.compose.components.SearchBar
+import com.compose.components.SectionText
+import com.compose.jetapp.model.Category
+import com.compose.jetapp.model.dummyCategory
 import com.compose.jetapp.ui.theme.JetAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +36,8 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -49,8 +56,31 @@ fun Banner(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        horizontalArrangement  = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(16.dp)
+    ){
+        items(dummyCategory, key = {it.textCategory}) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryRowPreview(){
+    JetAppTheme {
+        CategoryRow()
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
 fun DefaultPreview() {
     JetAppTheme {
         JetCoffeeApp()
