079a0caed7071f522b231a8573347fca4e8cf973
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-02-03 23:19:22 +0700

    [Bottom Item] Update MainActivity

--- app/src/main/java/com/compose/jetapp/MainActivity.kt
+++ app/src/main/java/com/compose/jetapp/MainActivity.kt
@@ -7,6 +7,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -17,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import com.compose.components.CategoryItem
 import com.compose.components.SearchBar
 import com.compose.components.SectionText
+import com.compose.jetapp.model.BottomBarItem
 import com.compose.jetapp.model.Category
 import com.compose.jetapp.model.dummyCategory
 import com.compose.jetapp.ui.theme.JetAppTheme
@@ -67,6 +76,46 @@ fun CategoryRow(
     ){
         items(dummyCategory, key = {it.textCategory}) { category ->
             CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+){
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            )
+        )
+
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                onClick = {}
+            )
         }
     }
 }
