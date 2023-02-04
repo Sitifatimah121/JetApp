3e888807aeccce9ae73338f32d7f0cad41406273
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-02-04 21:14:36 +0700

    [Menu Item] updated main

--- app/src/main/java/com/compose/jetapp/MainActivity.kt
+++ app/src/main/java/com/compose/jetapp/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
@@ -23,10 +22,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.components.CategoryItem
+import com.compose.components.MenuItem
 import com.compose.components.SearchBar
-import com.compose.components.SectionText
 import com.compose.jetapp.model.BottomBarItem
-import com.compose.jetapp.model.Category
+import com.compose.jetapp.model.Menu
 import com.compose.jetapp.model.dummyCategory
 import com.compose.jetapp.ui.theme.JetAppTheme
 
@@ -42,12 +41,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column {
-        Banner()
-        SectionText(stringResource(R.string.section_category))
-        CategoryRow()
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Scaffold (
+        bottomBar = { BottomBar()}
+    ){
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            Banner()
+
+        }
     }
+//    Column {
+//        Banner()
+//        SectionText(stringResource(R.string.section_category))
+//        CategoryRow()
+//    }
 }
 
 @Composable
@@ -62,6 +72,22 @@ fun Banner(
             modifier = Modifier.height(160.dp)
         )
         SearchBar()
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+){
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ){
+        items(listMenu, key = {it.title}) { menu ->
+            MenuItem(menu)
+        }
     }
 }
 
